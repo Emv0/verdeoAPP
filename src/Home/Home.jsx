9fa02ba0dfcdaf0f94388c8bbd58d516a35830d7
desarrollo <desarrollo@academicos.cesde.edu.co>
2024-04-16 20:50:09 -0500

    feat Se crea banner verdeoAPP

---
+++ src/Home/Home.jsx
@@ -0,0 +1,11 @@
+import "./Home.css"
+export function Home(){
+    return(
+        <>
+            <section className="banner text-white">
+                <h2>Verdeo APP</h2>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Odit voluptatibus suscipit ipsa facere. Incidunt blanditiis adipisci a laudantium. Dolorem, amet temporibus. Quo, voluptates iste. Animi iusto repellendus incidunt harum sint!</p>
+            </section>
+        </>
+    )
+}
